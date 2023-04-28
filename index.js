f3bbb41e9906596fbf9aa982aad3d7bec0af5036
MikhailV89 <114851579+MikhailV89@users.noreply.github.com>
2023-04-28 12:19:13 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+num1 = +prompt('Write first number?');
+num2 = +prompt('Write second number?');
+sum = num1 + num2;
+minus = num1 - num2;
+multiply = num1 * num2;
+divide = num1 / num2;
+
+alert(`Result:
+       Sum ${sum};
+       Minus ${minus};
+       Multiply ${multiply};
+       Divide ${divide};
+`);
+
